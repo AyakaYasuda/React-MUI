4a980ad4d405799d10667d14eda2fc0b3b87753d
Ayaka Yasuda <ayakayasuda23@gmail.com>
2021-12-23 21:33:02 -0800

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import ImageCard from "./components/ImageCard";
+
+const App = () => {
+  return (
+    <div className="App">
+      <ImageCard />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ImageCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ImageCard = () => {
+    return (
+        <div>
+            Hello from Image Card
+        </div>
+    )
+}
+
+export default ImageCard
